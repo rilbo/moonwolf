5faccdc9aa6bc9f917d2c24941c43fd00ccec6b4
veronique <veroniqueaero@gmail.com>
2021-12-02 17:08:17 +0800

    Ajouter page thank

---
+++ thank.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <title>Document</title>
+</head>
+<body>
+    <article id="thank">
+        <img src="assets/img/LOGO.png" alt="premier pas">
+        <h1>Merci pour votre inscription!</h1>
+        
+        <div class="thank-icons">
+        <a target="_blank" href="https://www.facebook.com/Premier-Pas-103213425532596">
+            <img src="assets/img/logo_facebook.png" alt="facebook"/>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/premierpas_off/">
+            <img src="assets/img/logo_instagram.png" alt="instagram"/>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/company/premier-pas-off/">
+            <img src="assets/img/logo_linkedin.png" alt="linkedin"/>
+        </a>
+        </div>
+       
+    </article>
+    
+</body>
+</html>
