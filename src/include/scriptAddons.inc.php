56b2de5529e46610ee239df35ef511feb77cfc03
Quentinqv <quentin010801@gmail.com>
2021-12-02 09:37:48 +0100

    Modification du script du plugin messenger

--- src/include/scriptAddons.inc.php
+++ src/include/scriptAddons.inc.php
@@ -6,27 +6,26 @@
 <div id="fb-root"></div>
 
 <!-- Your Plugin de discussion code -->
-<div id="fb-customer-chat" class="fb-customerchat"></div>
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
 
 <script>
-  var chatbox = document.getElementById("fb-customer-chat");
+  var chatbox = document.getElementById('fb-customer-chat');
   chatbox.setAttribute("page_id", "103213425532596");
   chatbox.setAttribute("attribution", "biz_inbox");
 
   window.fbAsyncInit = function() {
     FB.init({
-      xfbml: true,
-      version: "v12.0",
+      xfbml            : true,
+      version          : 'v12.0'
     });
   };
 
   (function(d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
+    var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js";
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js';
     fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
+  }(document, 'script', 'facebook-jssdk'));
 </script>
