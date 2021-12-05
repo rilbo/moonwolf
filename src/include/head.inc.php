8475cc223d0c9ae97d4f422a7bc5ce2a3203d7eb
Quentinqv <quentin010801@gmail.com>
2021-12-05 18:55:30 +0100

    Meta OG & Twitter

--- src/include/head.inc.php
+++ src/include/head.inc.php
@@ -2,9 +2,8 @@
   <meta charset="utf-8" />
   <title>Premier Pas</title>
   <link rel="icon" href="assets/img/favicon.ico" />
-  <link rel="stylesheet" href="assets/css/style.css" />
-  <script src="assets/js/main.js" defer></script>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
   <meta name="description" content="Premier Pas est un dispositif qui propose 3 programmes d’accompagnement en ligne pour aider et inciter les femmes à entreprendre dans le secteur de l'innovation digitale.">
   <meta name="author" content="Moonwolf">
 
@@ -15,21 +14,19 @@
   <meta property="og:site_name" content="Premier Pas" />
   <meta property="og:locale" content="fr_FR" />
 
-  <meta property="og:image" content="https://un-premier-pas.fr/LOGO _TEXTE.jpg" />
-  <meta property="og:image:secure_url" content="https://un-premier-pas.fr/LOGO _TEXTE.jpg" />
+  <meta property="og:image" content="http://un-premier-pas.fr/assets/img/LOGO_TEXTE.jpg" />
+  <meta property="og:image:secure_url" content="https://un-premier-pas.fr/assets/img/LOGO_TEXTE.jpg" />
   <meta property="og:image:type" content="image/png" />
   <meta property="og:image:width" content="300" />
   <meta property="og:image:height" content="300" />
   <meta property="og:image:alt" content="Premier Pas" />
 
-  <!---twitter card
-
-  <meta name="twitter:card" content="summary" />
-  <meta name="twitter:site" content="@yournameID" />
+  <!-- <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@un-premier-pas.fr" />
   <meta name="twitter:title" content="Premier Pas" />
   <meta name="twitter:description" content="Premier Pas est un dispositif qui propose 3 programmes d’accompagnement en ligne pour aider et inciter les femmes à entreprendre dans le secteur de l'innovation digitale." />
-  <meta name="twitter:image" content="https://un-premier-pas.fr/LOGO _TEXTE.jpg" />
-  <meta name="twitter:url" content="https://un-premier-pas.fr" />
-  
---->
+  <meta name="twitter:image" content="https://un-premier-pas.fr/assets/img/LOGO_TEXTE.jpg" />
+  <meta name="twitter:url" content="https://un-premier-pas.fr" /> -->
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <script src="assets/js/main.js" defer></script>
 </head>
