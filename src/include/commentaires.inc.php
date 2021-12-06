ae84ce53b73bcedf9d0d3df7a519e62948f8b9f8
Quentinqv <quentin010801@gmail.com>
2021-12-06 21:40:35 +0100

    Section Leurs avis

--- src/include/commentaires.inc.php
+++ src/include/commentaires.inc.php
@@ -1,42 +1,86 @@
 <section id="div__commentaires">
-  <div class="commentaires__global">
-    <h2>Commentaires</h2>
-    <div class="commentaire__item commentaire__item--gauche">
-      <div>
-        <p>
-          "Un accompagnement du début à la fin, qui m'a beaucoup aidé sur la
-          création de ma société"
-        </p>
-        <span>Marine, a fondé CrazyBottle</span>
+  <h2>Leurs avis</h2>
+  <div class="commentaires__container">
+    <div class="commentaires__container--first">
+      <div class="commentaires__each--global">
+        <div class="container__img">
+          <figure>
+            <picture>
+              <source srcset="assets/img/avis1.webp" type="image/webp">
+              <img src="assets/img/avis1.png" alt="Femme de face souriante">
+              <picture>
+          </figure>
+        </div>
+        <div class="commentaires__each--texte">
+          <h6>Marine, fondatrice de CrazyBottle</h6>
+          <p>Un accompagnement du début à la fin, qui m’a beaucoup aidé sur la création de ma société.</p>
+        </div>
+      </div>
+      <div class="commentaires__each--global">
+        <div class="container__img">
+          <figure>
+            <picture>
+              <source srcset="assets/img/avis2.webp" type="image/webp">
+              <img src="assets/img/avis2.png" alt="Femme de face souriante">
+              <picture>
+          </figure>
+        </div>
+        <div class="commentaires__each--texte">
+          <h6>Valentine, à crée la société Earbag</h6>
+          <p>La gestion de mon entreprise s’est nettement améliorée grâce aux experts et à leurs nombreux conseils.</p>
+        </div>
       </div>
     </div>
-    <div class="commentaire__item commentaire__item--droite">
-      <div>
-        <p>
-          "J'ai pu apprendre énormément avec Premier Pas, une équipe très
-          accueillante et très professionnelle"
-        </p>
-        <span>Nathalie, fondatrice de Piedor</span>
+    <div class="commentaires__container--second">
+      <div class="commentaires__each--global">
+        <div class="container__img">
+          <figure>
+            <picture>
+              <source srcset="assets/img/avis3.webp" type="image/webp">
+              <img src="assets/img/avis3.png" alt="Femme de face souriante">
+              <picture>
+          </figure>
+        </div>
+        <div class="commentaires__each--texte">
+          <h6>Nathalie, fondatrice de Piedor</h6>
+          <p>J’ai pu apprendre énormément avec Premier Pas, une équipe très accueuillante et très professionnelle.</p>
+        </div>
       </div>
-    </div>
-    <div class="commentaire__item commentaire__item--gauche">
-      <div>
-        <p>
-          "Étant une femme, je suis très heureuse de voir d'autres
-          personnalités féminines s'engager dans le combat de
-          l'entrepreneuriat"
-        </p>
-        <span>Lisa, a crée son agence Arob</span>
-      </div>
-    </div>
-    <div class="commentaire__item commentaire__item--droite">
-      <div>
-        <p>
-          "L'image de ma boite s'est nettement améliorée grâce aux expertes
-          et à leurs nombreux conseils."
-        </p>
-        <span>Valentine, Société Earbag</span>
+      <div class="commentaires__each--global">
+        <div class="container__img">
+          <figure>
+            <picture>
+              <source srcset="assets/img/avis4.webp" type="image/webp">
+              <img src="assets/img/avis4.png" alt="Femme de face souriante">
+              <picture>
+          </figure>
+        </div>
+        <div class="commentaires__each--texte">
+          <h6>Marine, fondatrice de CrazyBottle</h6>
+          <p>Un accompagnement du début à la fin, qui m’a beaucoup aidé sur la création de ma société.</p>
+        </div>
       </div>
     </div>
   </div>
+  <div class="commentaires__selector--slider">
+    <input type="radio" name="commentaires" id="commentaire1" checked>
+    <label for="commentaire1"></label>
+    <input type="radio" name="commentaires" id="commentaire2">
+    <label for="commentaire2"></label>
+  </div>
 </section>
+
+<script>
+  let inputRadio = document.getElementsByName("commentaires")
+  for (const input of inputRadio) {
+    input.addEventListener("change", function() {
+      if (inputRadio[0].checked) {
+        document.querySelector(".commentaires__container--first").style.display = "flex"
+        document.querySelector(".commentaires__container--second").style.display = "none"
+      } else if (inputRadio[1].checked) {
+        document.querySelector(".commentaires__container--second").style.display = "flex"
+        document.querySelector(".commentaires__container--first").style.display = "none"
+      }
+    })
+  }
+</script>
