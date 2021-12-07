8fc423880584eed4b50a0cc0611e31bdfd5c0140
veronique <veroniqueaero@gmail.com>
2021-12-07 11:08:24 +0800

    newsletter v6

--- src/include/newsletter.inc.php
+++ src/include/newsletter.inc.php
@@ -1,34 +1,28 @@
+
 <article id="newsletter">
-  <!-- Begin Mailchimp Signup Form -->
   <div id="mc_embed_signup">
-    <form action="https://gmail.us20.list-manage.com/subscribe/post?u=6d4892d2a4008d1148aba7142&amp;id=4ef5deedef" method="post" id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form" class="validate" target="_blank" onclick= "return avoidJump()">
-      <div id="mc_embed_signup_scroll">
         <h2>Newsletter</h2>
-
         <section>
-          <div class="mc-field-group">
-            <label for="mce-EMAIL">Addresse : </label>
-            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" />
-          </div>
 
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display: none"></div>
-            <div class="response" id="mce-success-response" style="display: none"></div>
-          </div>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <form action="https://gmail.us20.list-manage.com/subscribe/post?u=6d4892d2a4008d1148aba7142&amp;id=4ef5deedef" 
+                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                
+                <p>Vous souhaitez en savoir plus sur les entreprises que nous accompagnons, suivre le succès de ces 
+                  femmes qui osent entreprendre, alors inscrivez-vous à notre newsletter mensuelle.</p>
+                
+                <div class="mc-field-group">
+                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Entrer votre adresse e-mail" required>
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_6d4892d2a4008d1148aba7142_4ef5deedef" tabindex="-1" value="">
+                  </div>
 
-          <div style="position: absolute; left: -5000px" aria-hidden="true">
-            <input type="text" name="b_6d4892d2a4008d1148aba7142_4ef5deedef" tabindex="-1" value="" />
-          </div>
+                  <input type="submit" value="S'inscrire" name="subscribe" id="mc-embedded-subscribe" class="button">
+                  <div id="subscribe-result">
+                  </div>
+                </div>
+               
 
-          <div class="clear">
-            <input type="submit" value="S'inscrire" name="subscribe" id="mc-embedded-subscribe" class="button" />
-          </div>
+            </form>
         </section>
-      </div>
-    </form>
-  </div>
-
-  <!--End mc_embed_signup-->
+    </div>
 </article>
