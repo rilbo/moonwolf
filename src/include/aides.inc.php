1dbd60d79c9bb97a7c84be17f200c0602e57b03b
veronique <veroniqueaero@gmail.com>
2021-12-08 01:39:38 +0800

    ajouter 3paragraphe

--- src/include/aides.inc.php
+++ src/include/aides.inc.php
@@ -1,8 +1,55 @@
 <section id="div__aides_femmes">
-  <h2>Les aides<br> pour les femmes</h2>
-  <p>
-    En plus de nos équipe d'expertes, tout un tas d'aides sont à votre
-    disposition pour vous assister durant votre développement.
-  </p>
-  <a href="#" class="btn" id="btn__aides">Voir plus</a>
+  <div class="aide_item">
+    <h2>Les aides<br> pour les femmes</h2>
+    <p>
+      En plus de nos équipe d'expertes, tout un tas d'aides sont à votre
+      disposition pour vous assister durant votre développement.
+    </p>
+    <a href="javascript:void(0)"  class="btn" id="btn__aides">Voir plus</a>
+    <a href="javascript:void(0)"  class="btn" id="btn__aides_fermer">Fermez</a>
+
+</div>
+
+  <div id="aide_article">
+      <article>
+         <h3>La jfd</h3>
+         <p>La journée de la femme digitale (JFD) est le rendez-vous féminin de l'innovation. L'objectif : mettre à l'honneur les femmes chefs d'entreprise ou occupant des postes de direction dans les nouvelles technologies.
+            <br><br>Les lauréates bénéficieront de l'exposition médiatique de la JFD, de soutiens financiers, d’une valeur équivalente à 1 millions d’euros.</p>
+      </article>
+      <article>
+         <h3>Journée des femmes entrepreneuses</h3>
+         <p>La journée des femmes entrepreneures est organisée chaque année en marge du Salon SME, un événement ayant lieu à Paris et dédié aux dirigeants et créateurs de start-ups ou de TPE/PME.
+            <br><br>La journée de la femme entrepreneure propose durant une journée de nombreuses conférences et ateliers pour encourager et accompagner les femmes porteuses d’un projet.</p>
+      </article>
+      <article>
+         <h3>Prêt d’honneur</h3>
+         <p>Il s’agit d’un prêt d’honneur accordé sans demande de garantie personnelle ni intérêts par le réseau Initiative France. Il permet aux femmes entrepreneures de renforcer leurs fonds propres et ainsi d’accéder plus facilement à des prêts plus importants. Sans remplacer l’emprunt, il permet d’en simplifier la démarche grâce à un effet de levier.
+</p>
+      </article>
+  </div>
+  
+  
 </section>
+
+
+<!--
+<section id="div__aides_femmes_article">
+  <div class="aide_item">
+      <article>
+         <h3>La jfd</h3>
+         <p>La journée de la femme digitale (JFD) est le rendez-vous féminin de l'innovation. L'objectif : mettre à l'honneur les femmes chefs d'entreprise ou occupant des postes de direction dans les nouvelles technologies.
+            <br><br>Les lauréates bénéficieront de l'exposition médiatique de la JFD, de soutiens financiers, d’une valeur équivalente à 1 millions d’euros.</p>
+      </article>
+      <article>
+         <h3>Journée des femmes entrepreneuses</h3>
+         <p>La journée des femmes entrepreneures est organisée chaque année en marge du Salon SME, un événement ayant lieu à Paris et dédié aux dirigeants et créateurs de start-ups ou de TPE/PME.
+            <br><br>La journée de la femme entrepreneure propose durant une journée de nombreuses conférences et ateliers pour encourager et accompagner les femmes porteuses d’un projet.</p>
+      </article>
+      <article>
+         <h3>Prêt d’honneur</h3>
+         <p>Il s’agit d’un prêt d’honneur accordé sans demande de garantie personnelle ni intérêts par le réseau Initiative France. Il permet aux femmes entrepreneures de renforcer leurs fonds propres et ainsi d’accéder plus facilement à des prêts plus importants. Sans remplacer l’emprunt, il permet d’en simplifier la démarche grâce à un effet de levier.
+</p>
+      </article>
+  </div>
+</section>
+--->
