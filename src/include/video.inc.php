5b8626b831898a0f747f98e86649460cbc278e7e
valentin <valmuller14@gmail.com>
2021-12-09 10:07:09 +0100

    ajout placeholder video

--- src/include/video.inc.php
+++ src/include/video.inc.php
@@ -1,3 +1,3 @@
 <section id="div__video">
-  <video src="assets/img/video.mp4" muted controls></video>
+  <video src="assets/img/video.mp4" poster="assets/img/placeholder.png" muted controls></video>
 </section>
